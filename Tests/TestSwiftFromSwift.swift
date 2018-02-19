547c29f9827ab50596d6c4e6983a4d80e5fcb661
Maksym Grebenets <maksym.grebenets@cba.com.au>
2018-02-19 15:40:49 +1100

    Add extra test that prevents compiler from optimizing Swift code out

--- Tests/TestSwiftFromSwift.swift
+++ Tests/TestSwiftFromSwift.swift
@@ -9,4 +9,16 @@ class TestSwiftFromSwift: XCTestCase {
         let data = "abc".data(using: .utf8)! as NSData
         XCTAssert(data.hexadecimalNSString() == "")
     }
+
+    // If the code below is commented, then hexadecimalString
+    // is considered to be never used by the compiler.
+    // It will then optimize it out of the final binary!!!
+    // Even if -Onone is set for SWIFT and GCC optimization levels!
+    // Trying to call this method via hexadecimalNSString "wrapper" will
+    // cause a crash then!
+    // Not sure if same happens for dynamic frameworks...
+    func testSwiftFromSwift2() {
+        let data = "abc".data(using: .utf8)!
+        XCTAssert(data.hexadecimalString() == "")
+    }
 }
